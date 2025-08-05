8149295b72258ef6e08f0b13707a70ca41d030aa
GovinduAbhiram <abhiramstu501@gmail.com>
2025-08-05 11:13:20 +0530

    Add files via upload

---
+++ SimpleCalculator.py
@@ -0,0 +1,16 @@
+#simple calculator for two Numbers
+num1=int(input("Give value of number 1: "))
+num2=int(input("Give value of number 2: "))
+operator=input("Give a operator: ")
+if operator=="+":
+    print(f"Addition of Two numbers {num1+num2}")
+elif operator=="-":
+    print(f"Subtraction of Two numbers {num1-num2}")
+elif operator=="*":
+    print(f"Multiplication of Two numbers {num1*num2}")
+elif operator=="/":
+    print(f"Divison of Two numbers {num1/num2}")
+elif operator=="**":
+    print(f"Squaring of a Number {num1**num2}")
+else:
+    print("Not Valid")
